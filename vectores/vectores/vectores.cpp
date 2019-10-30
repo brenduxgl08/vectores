cfb07f63f60cce39a8c0a0354d99bbc8166ce5a8
Alumno <Alumno@PC10>
2019-10-30 11:43:57 -0600

    completo

--- vectores/vectores/vectores.cpp
+++ vectores/vectores/vectores.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main()
 {
-   
+	int numero[5]; //Array o vector de 5 números
+	int suma; // Un entero que guardará la suma
+	suma = 0;
+	for (int i = 0; i <= 5; i++)
+	{
+		cout << "Introduce un numero: ";
+		cin >> numero[i];
+		suma = suma + numero[i];
+	}
+	cout << "La suma es: " << suma;
+	return 0;
 }
